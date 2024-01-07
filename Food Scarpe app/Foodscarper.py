9081c103f77f7f3cf51be1fdf0496ffda336ae80
ErrorCode00x404 <144743228+ErrorCode00x404@users.noreply.github.com>
2024-01-06 20:47:14 -0600

    Add files via upload

---
+++ Food Scarpe app/Foodscarper.py
@@ -0,0 +1,19 @@
+# Prompt user to create an account. 
+
+
+
+
+
+# Prompt user to put the location via zip code.
+
+
+
+
+
+# Get all fast food restaurant in the zip code.  
+
+
+
+
+
+# If there are more than 1 of the same restaurant it will find which location has better deals and the coupons. 
